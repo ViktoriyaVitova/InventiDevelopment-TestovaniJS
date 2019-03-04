c3c37c8a0fb231f6292d72425c704f3e28eb4085
Martin Macháček <martin.machacek@inventi.cz>
2019-03-04 19:04:01 +0100

    Implemented first unit and API tests

--- Calculator/my-test.spec.js
+++ Calculator/my-test.spec.js
@@ -1,5 +1,32 @@
 import test from "ava";
+import axios from "axios";
+import { plus } from "./JS";
 
-test("test my test", t => {
-  // Insert your first test
+test("plus function works with simple numbers", t => {
+  t.is(plus(2, 2), 4);
+});
+
+test("users endpoint returns 200", async t => {
+  const response = await axios.get("http://localhost:3000/users");
+
+  t.is(response.status, 200);
+});
+
+test("users endpoint returns array", async t => {
+  const response = await axios.get("http://localhost:3000/users");
+
+  t.true(Array.isArray(response.data));
+});
+
+test("users endpoint returns objects with correct stucture", async t => {
+  const response = await axios.get("http://localhost:3000/users");
+
+  const firstUser = response.data[0];
+
+  t.true(firstUser.id > 0);
+  t.truthy(firstUser.firstName);
+  t.truthy(firstUser.lastName);
+  t.truthy(firstUser.email);
+  t.truthy(firstUser.age);
+  t.truthy(firstUser.companyId);
 });
