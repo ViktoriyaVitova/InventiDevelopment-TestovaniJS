90df74a8748a51dec88e9adc8dc7194514eeec01
Martin Macháček <martin.machacek@inventi.cz>
2019-03-04 16:54:17 +0100

    Review code, prepare for testing

--- Calculator/JS.js
+++ Calculator/JS.js
@@ -1,61 +1,58 @@
-function plus () {
-/// plus two numbers    
-    var num1, num2, result;
-   //change  
-    num1 = document.getElementById('n1').value;
-    num1 = parseInt(num1);
-
-    num2 = document.getElementById('n2').value;
-    num2 = parseInt(num2);
-    
-    result = num1 + num2;
-    document.getElementById("out").innerHTML = result;
+/* Helper method for receiving number from HTML input */
+function getNumberFromInput(input) {
+  return parseInt(document.getElementById(input).value);
 }
 
-function minus (){
-/// minus 2 numbers
-    var num1, num2, result;
-   
-    num1 = document.getElementById('n1').value;
-    num1 = parseInt(num1);
-
-    num2 = document.getElementById('n2').value;
-    num2 = parseInt(num2);
-
-    result = num1 - num2;
-
-    document.getElementById("out").innerHTML = result;
-
+/* Helper method for writing result to HTML */
+function writeResult(result) {
+  document.getElementById("out").innerHTML = result;
 }
 
-function multiply (){
-    /// multiply 2 numbers
-        var num1, num2, result;
-       
-        num1 = document.getElementById('n1').value;
-        num1 = parseInt(num1);
-    
-        num2 = document.getElementById('n2').value;
-        num2 = parseInt(num2);
-    
-        result = num1 * num2;
-    
-        document.getElementById("out").innerHTML = result;
-    
-    }
+/* Logic */
+function plus(num1, num2) {
+  return num1 + num2;
+}
 
-    function divide (){
-        /// devide 2 numbers
-            var num1, num2, result;
-           
-            num1 = document.getElementById('n1').value;
-            num1 = parseInt(num1);
-        
-            num2 = document.getElementById('n2').value;
-            num2 = parseInt(num2);
-        
-            result = num1/num2;
-        
-            document.getElementById("out").innerHTML = result;
-        
-        }
+function minus(num1, num2) {
+  return num1 - num2;
+}
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function divide(num1, num2) {
+  return num1 / num2;
+}
+
+/* Buttons click handler */
+function calculate(operation) {
+  const num1 = getNumberFromInput("n1");
+  const num2 = getNumberFromInput("n2");
+
+  switch (operation) {
+    case "plus":
+      writeResult(plus(num1, num2));
+      break;
+
+    case "minus":
+      writeResult(minus(num1, num2));
+      break;
+
+    case "multiply":
+      writeResult(multiply(num1, num2));
+      break;
+
+    case "divide":
+      writeResult(divide(num1, num2));
+      break;
+  }
+}
+
+module.exports = {
+  plus,
+  minus,
+  multiply,
+  divide,
+  calculate
+};
